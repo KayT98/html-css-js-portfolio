333aa4871ffea57ca887a0a63a2d6dcb551525da
KayT98 <khanhtruong109@yahoo.com>
2023-09-18 22:57:26 -0500

    added slider and new page

--- javascript/main.js
+++ javascript/main.js
@@ -12,3 +12,54 @@ function error() {
         alert("You are good to go :)")
     }
 }
+
+
+setInterval(() =>{
+
+const time = new Date();
+const current_time = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
+
+document.getElementById('time').innerHTML = current_time
+}, 1000)
+
+const images = document.querySelectorAll('#slider img');
+const previousImage = document.getElementById("prev");
+const nextImage = document.getElementById("next");
+let currentIndex = 0;
+function reset() {
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.remove('active');
+  }
+}
+function initializeSlider() {
+  reset();
+  images[currentIndex].classList.add('active');
+}
+function slideLeft() {
+  reset();
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  images[currentIndex].classList.add('active');
+}
+
+function slideRight() {
+  reset();
+  currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  images[currentIndex].classList.add('active');
+}
+initializeSlider();
+
+previousImage.addEventListener('click', function() {
+  slideLeft();
+});
+
+nextImage.addEventListener('click', function() {
+  slideRight();
+});
+
+
